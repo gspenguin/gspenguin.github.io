d32891559e9260cf1948234fd04e602099444570
gspenguin <97851722+gspenguin@users.noreply.github.com>
2022-01-16 18:26:25 -0500

    Fixing JS errors

--- cookieform.js
+++ cookieform.js
@@ -1,15 +1,17 @@
 function calculateTotals(){
     let total = 0;
-    document.querySelector('input.cookie').forEach(cookie => {
-        const boxes = parseInt(cookie.value, 10);
-        if(cookie.name === 'Toffeetastic' || cookie.name === 'Girl Scout Smores'){
-            total = boxes * 6 + total;
-        } else {
-            total = boxes * 5 + total;
+    document.querySelectorAll('input.cookie').forEach(cookie => {
+        if(cookie.value){
+            const boxes = parseInt(cookie.value, 10);
+            if(cookie.name === 'Toffeetastic' || cookie.name === 'Girl Scout Smores'){
+                total = boxes * 6 + total;
+            } else {
+                total = boxes * 5 + total;
+            }
         }
-    })
+    });
     document.getElementById('Total').value = total;
 }
-document.querySelector('input.cookie').forEach(input => {
+document.querySelectorAll('input.cookie').forEach(input => {
     input.addEventListener('change', calculateTotals);
 });
